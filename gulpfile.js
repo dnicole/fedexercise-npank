8c4741c6b099e332e085e1212f99bf6b1e3b527f
nicole pank <nicole.pank@gmail.com>
2015-08-04 23:50:19 -0500

    deployment tweaks

--- gulpfile.js
+++ gulpfile.js
@@ -24,14 +24,8 @@ gulp.task('watch', function() {
     gulp.watch('js/*.js', ['lint', 'scripts']);
 });
 
-// gulp.task('serveprod', function() {
-//   connect.server({
-//     root: 'https://dry-beyond-7204.herokuapp.com',
-//     port: process.env.PORT || 5000, // localhost:5000
-//     livereload: false
-//   });
-// });
-
 // Default Task
 gulp.task('default', ['lint', 'scripts', 'watch']);
+
+// I run on deploy!
 gulp.task('server', ['scripts']);
