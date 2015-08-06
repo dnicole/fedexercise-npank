44e935c6e20966bd4f297c91666fd46fd0e4435e
nicole pank <nicole.pank@gmail.com>
2015-08-05 20:35:30 -0500

    document load first

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
-function init() {
-    console.log('loaded');
-    var searchButton = document.getElementById('search');
-    searchButton.onclick = showAlert;
-};
+window.addEventListener('load',
+    function init() {
+        console.log('loaded');
+        var searchButton = document.getElementById('search');
+        searchButton.onclick = showAlert;
+    });
+    false;
+
 
 function showAlert(e) {
     e.preventDefault();
     alert("onclick Event detected!");
 }
 
-init();
