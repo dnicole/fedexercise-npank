82fb84dc7d2b69cf1d03485559b18904975fce5b
nicole pank <nicole.pank@gmail.com>
2015-08-06 21:24:55 -0500

    add most of response processing

--- js/index.js
+++ js/index.js
@@ -1,36 +1,68 @@
-var searchButton,
-    searchField,
+var jsSearchButton,
+    jsSearchField,
+    imgTag,
+    jsResults,
     apiKey = 'a5e95177da353f58113fd60296e1d250',
     userId = '132365033@N08';
 
 window.addEventListener('load', function init() {
-    searchButton = document.getElementById('searchButton');
-    searchField = document.getElementById('searchField');
-    searchButton.addEventListener('click', callSearchFlickr);
+    jsSearchButton = document.getElementById('jsSearchButton');
+    jsSearchField = document.getElementById('jsSearchField');
+    jsResults = document.getElementById('jsResults');
+    jsSearchButton.addEventListener('click', callSearchFlickr);
 });
 
 function callSearchFlickr(e) {
     e.preventDefault();
-    // alert('who\'s going chicken hunting?');
-    var searched = searchField.value;
 
-    console.log(searched);
-    searchField.value = '';
-    console.log(searched);
+    var searched = jsSearchField.value;
+
+    if (jsResults.index >= 1) {
+        console.log('clearing old results');
+        jsResults.clear();
+    } else {
+        console.log('nothing to clear');
+    }
+
+    jsSearchField.value = '';
+
     client(searched, apiKey, userId);
 }
 
 var client = function (searched, apiKey, userId) {
+    var res;
+    imgTag = searched;
+
     // Hello giant string!
     var endpoint = 'https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=' +
         apiKey +
         '&user_id=' +
         userId +
-        '&format=json&nojsoncallback=1';
+        '&format=json&nojsoncallback=1&tags=' +
+        imgTag;
+
+    // Create AJAX request
     var searchFlickr = new XMLHttpRequest();
+
+    // Send it off!
     searchFlickr.open('get', endpoint, true);
+
+    searchFlickr.onload = function() {
+        // Process results
+        if (searchFlickr.status >= 200 && searchFlickr.status < 400) {
+            res = JSON.parse(searchFlickr.responseXML);
+            showPics(res);
+        } else {
+            var err = document.createTextNode('Sorry, something went wrong.');
+            jsResults.appendChild(err);
+            throw new Error('No pictures for you!');
+        }
+    };
+
     searchFlickr.send();
 };
 
-
+var showPics = function (res) {
+    console.log(res);
+};
 
